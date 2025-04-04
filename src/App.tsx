da034cbfd97718ad582c4b8abeb488ded07a4d13
Yash Zinzuwadia <yash.zinzuwadia.2024@gmail.com>
2025-04-04 17:37:06 +0530

    Added: chakra ui setup and initial routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Chats from "./pages/Chats";
+
 function App() {
-    return <div>hello</div>;
+    return (
+        <div>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/chats" element={<Chats />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
